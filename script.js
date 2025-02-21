db2cd24f3f61b52c4f53724ad9da71c0362c2c02
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-21 17:15:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', function() {
+      const statusElement = document.getElementById('status');
+      statusElement.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
+    });
